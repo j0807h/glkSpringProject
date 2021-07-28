42df6a2f790a69db0ed49945f7d050043c4aa00d
gfdfg <gfdfg@112.172.131.143>
2021-07-28 10:21:38 +0900

    프로젝트 생성

---
+++ glk-sd-board/src/main/java/com/dj/sd/GlkSdBoardApplication.java
@@ -0,0 +1,13 @@
+package com.dj.sd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlkSdBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlkSdBoardApplication.class, args);
+	}
+
+}
---
+++ glk-sd-board/src/test/java/com/dj/sd/GlkSdBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dj.sd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlkSdBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
