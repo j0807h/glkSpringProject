3fc792c1e8bfbf97d8d35411196b0e71df1d51a0
gfdfg <gfdfg@112.172.131.143>
2021-07-28 11:32:33 +0900

    mariaDB연동과 configuration 설정

---
+++ glk-sd-board/src/main/java/com/dj/sd/config/DatabaseConfiguration.java
@@ -0,0 +1,20 @@
+package com.dj.sd.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DatabaseConfiguration {
+	
+	@Bean
+	@ConfigurationProperties(prefix = "spring.datasource") 
+		// DB 연동을 위함. application.properties에서 spring.datasource라는 이름으로 사용.
+	public DataSource dataSource() {
+		return DataSourceBuilder.create().build();
+	}
+	
+}
---
+++ glk-sd-board/src/main/java/com/dj/sd/config/MybatisConfiguration.java
@@ -0,0 +1,42 @@
+package com.dj.sd.config;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/*
+ * Mybatis 설정
+ * @author JeongHwa
+ * */
+@Configuration
+@MapperScan(basePackages = "com.jh.sp.mvc.repository")
+public class MybatisConfiguration {
+	
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(@Autowired DataSource dataSource, ApplicationContext applicationContext) throws Exception, SQLException {
+		SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+		factoryBean.setDataSource(dataSource);
+		factoryBean.setMapperLocations(applicationContext.getResources("classpath:mybatis/sql/*.xml"));
+		
+		//CamelCase 적용
+		SqlSessionFactory factory = factoryBean.getObject();
+		factory.getConfiguration().setMapUnderscoreToCamelCase(true);
+		
+		return factoryBean.getObject();
+	}
+	
+	@Bean
+	public SqlSessionTemplate sessionTemplate(@Autowired SqlSessionFactory sqlSessionFactory) {
+		return new SqlSessionTemplate(sqlSessionFactory);
+	}
+	
+}
